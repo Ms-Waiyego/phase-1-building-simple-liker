0f80c6afc8d737a82bcfc8a06334492eac8e88c2
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-09-06 10:37:01 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,10 +3,48 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const init = () => {
+  let modal = document.getElementById("modal");
+  let modalMessage = document.getElementById("modal-message");  
 
+  let likeElements = document.querySelectorAll(".like-glyph");
+ 
+  for(const like of likeElements){
+    like.addEventListener("click",(() => {
 
+      if(like.classList.contains("activated-heart")){
+        mimicServerCall()
+          .then(() => { 
+            like.classList.remove("activated-heart");
+            like.innerHTML = EMPTY_HEART;
+            alert(`You unliked a post`);
 
+          })
+          .catch((err) => {
+            modal.classList.remove("hidden");
+           modalMessage.innerHTML = err.message;
 
+        setTimeout(()=> {
+           modal.classList.add("hidden");
+         },3000);
+
+          })
+      } else {
+        mimicServerCall()
+          .then(() => { 
+            like.classList.add("activated-heart");
+            like.innerHTML = FULL_HEART;
+            alert("You liked A post");           
+          })
+        .catch((err) => {
+         modal.classList.remove("hidden")
+        modalMessage.innerHTML = err.message;
+            
+        setTimeout(()=> {
+        modal.classList.add("hidden");
+            },3000); })
+      } }))};}
+document.addEventListener("DOMContentLoaded",init);
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
